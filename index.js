d3e84a8df2039189a584d61573c21418081c323c
Febin Bellamy <febinbellamy@gmail.com>
2022-05-28 19:20:34 -0400

    Pokemon route

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const app = express();
-const pokemonRoutes = require("./routes/pokemon");
-
 const port = 3500;
-
+const morgan = require('morgan');
+const pokemonRoutes = require("./routes/pokemon");
 app.use(express.json()) 
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.send('Welcome to the RESTful Pokémon API!'));
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<html><body><h1>Welcome to the RESTful Pokemon API!</h1></body></html>");
+  });
 
 app.use('/pokemon', pokemonRoutes);
-
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
 
-// app.get('*', (req, res) => {
-//   res.send(`I don't know that path!`)
-//   // catch all error handler
-// })
+// app.all("*", (req, res, next) => {
+//     next(new ExpressError("Page Not Found", 404));
+//   });
+  
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,13 +1,19 @@
-// /pokemon route
-
-const express = require('express');
+const express = require("express");
 const app = express();
-
 const router = express.Router();
+const data = require('../data');
+let pokemon = data;
 
+router.get("/", (req, res) => { // get all available pokemon
+    res.send(pokemon);
+});
 
-router.get('/', (req, res) => {
-    res.send('hello')
+// get single pokemon by ID
+router.get('/:id', (req, res) => {
+    console.log(req.params);
+    const { id } = req.params;
+    const singlePokemon = pokemon.results.find((pokemon) => pokemon.id === id);
+    res.send(singlePokemon);
 })
 
 module.exports = router;
